54f4bafcefe579c513fc1a4da0b01f3ff3357382
github RenaHayashi <rena.hayashi.route66@gmail.com>
2020-07-26 10:14:54 +0900

    参考一覧

--- my_app/lib/routes/timeline_route.dart
+++ my_app/lib/routes/timeline_route.dart
@@ -12,15 +12,127 @@ class Timeline extends StatelessWidget {
 //          child: Icon(Icons.settings),
 //        ),
         actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.add),
+          new FlatButton(
+            child: const Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {
+              Navigator.of(context).pushNamed("/talk");
+            },
           ),
         ],
         centerTitle: true,
       ),
-      body: Container(
-        child: Text("参考書一覧"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(10),
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            child: Text(
+              "学習中の参考書",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Card(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 90,
+                    height: 150,
+                    child: Text(
+                      "Card1",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 90,
+                    height: 150,
+                    child: Text(
+                      "Card2",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 90,
+                    height: 150,
+                    child: Text(
+                      "Card3",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            child: Text(
+              "先輩たちの勉強をチェック！",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Card(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 90,
+                    height: 150,
+                    child: Text(
+                      "Card1",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 90,
+                    height: 150,
+                    child: Text(
+                      "Card2",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 90,
+                    height: 150,
+                    child: Text(
+                      "Card3",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
